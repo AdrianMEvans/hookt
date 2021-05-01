249294f1806ec49fc0f5673eec3eb187a1b336e0
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-05-01 20:09:08 +0800

    npm install --save react-native-gesture-handler

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 
+import * as firebase from 'firebase';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -17,4 +19,19 @@ export default function App() {
           </Stack.Navigator>
         </NavigationContainer>
   );
+}
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBktqiOWC1IeP-IuFjnQR_2Hvw6BvrrAS4",
+  authDomain: "hookt-app.firebaseapp.com",
+  projectId: "hookt-app",
+  storageBucket: "hookt-app.appspot.com",
+  messagingSenderId: "785873493026",
+  appId: "1:785873493026:web:957bdaca0eee302823be7b",
+  measurementId: "G-45DYE0PX9F"
+};
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig)
 }
