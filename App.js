af6011fb9c7c3411146820234765426c74f50ad0
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-05-01 21:38:06 +0800

    added Signout func and fixed syntax error

--- App.js
+++ App.js
@@ -1,28 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 
-import * as firebase from 'firebase';
+import { View, Text, Button, StyleSheet } from 'react-native'
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import * as firebase from 'firebase'
 
-import LandingScreen from './components/auth/Landing'
-import RegisterScreen from './components/auth/Register'
-import LoginScreen from './components/auth/Login'
-
-
-const Stack = createStackNavigator();
-export default function App() {
-  return (
-    <NavigationContainer>
-          <Stack.Navigator initialRouteName="Landing">
-            <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="Register" component={RegisterScreen} />
-            <Stack.Screen name="Login" component={LoginScreen} />
-          </Stack.Navigator>
-        </NavigationContainer>
-  );
-}
 
 const firebaseConfig = {
   apiKey: "AIzaSyBktqiOWC1IeP-IuFjnQR_2Hvw6BvrrAS4",
@@ -37,3 +19,83 @@ const firebaseConfig = {
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig)
 }
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/Register'
+import LoginScreen from './components/auth/Login'
+
+const Stack = createStackNavigator();
+
+export class App extends Component {
+
+  
+  constructor(props) {
+    super()
+    this.state = {
+      loaded: false,
+    }
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        this.setState({
+          loggedIn: false,
+          loaded: true,
+        })
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true,
+        })
+      }
+    })
+  }
+  render() {
+    const onLogout = () => {
+      firebase.auth().signOut();
+  }
+    const { loggedIn, loaded } = this.state;
+    if (!loaded) {
+      return (
+        <View style={styles.loading}>
+          <Text>Loading</Text>
+        </View>
+      )
+    }
+
+    if (!loggedIn) {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Landing">
+            <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+    }
+    return (
+      <View style={styles.loading}>
+          <Text>User Logged In!!!</Text>
+          <Button 
+          title="Logout"
+          onPress={() => onLogout()}
+          />
+        </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  loading: {
+    flex: 1,
+  //   alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default App
