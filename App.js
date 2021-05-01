6f0b5871b71f5cd798b5570a52cdaeb81b668fc7
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-05-01 16:01:05 +0800

    added Register Page

--- App.js
+++ App.js
@@ -1,28 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet } from 'react-native';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/Register'
 
 const Stack = createStackNavigator();
 export default function App() {
   return (
-    <NavigationContainer styles={styles.navigationContainer}>
-      <Stack.Navigator initialRouteName="Landing">
-        <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }}/>
-
-      </Stack.Navigator>
-    </NavigationContainer>
+    <NavigationContainer>
+          <Stack.Navigator initialRouteName="Landing">
+            <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  navigationContainer: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- components/auth/Landing.js
+++ components/auth/Landing.js
@@ -1,22 +1,15 @@
 import React from 'react'
-import { Text, View, Button, StyleSheet } from "react-native";
+import { Text, View, Button } from 'react-native'
 
 export default function Landing({ navigation }) {
     return (
-        <View styles={styles.landingPage}>
-            <Button 
-            title="Register"
-            onPress={() => navigation.navigate("Register")}/>
-             <Button 
-            title="Login"
-            onPress={() => navigation.navigate("Login")}/>
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+            <Button
+                title="Register"
+                onPress={() => navigation.navigate("Register")} />
+            <Button
+                title="Login"
+                onPress={() => navigation.navigate("Login")} />
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    landingPage: {
-      flex: 1,
-      justifyContent: 'center',
-    },
-  });
