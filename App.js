01d6a6ab52cfc8fcfd99c5cb7ce2f925b4dd9f79
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-05-01 15:23:33 +0800

    import variable createStackNavigator

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import LandingScreen from './components/auth/Landing'
 
 const Stack = createStackNavigator();
