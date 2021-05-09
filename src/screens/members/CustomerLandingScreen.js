12d5b90042f878d7bdd0adf8b256b0755a447107
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-05-09 20:11:04 +0800

    MembersScreens Added

--- src/screens/members/CustomerLandingScreen.js
+++ src/screens/members/CustomerLandingScreen.js
@@ -1,18 +1,42 @@
 import React from 'react'
-import { Text, View, Button } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import { Text, Button } from 'react-native-elements';
+
+import Spacer from '../../components/Spacer'
 
 
-export default function CustomerLanding({ navigation }) {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center' }}>
-            <Button 
-            title="CreateCard"
-            onPress = {() => navigation.navigate("CreateLoyaltyCard")}
-            />
-            <Button 
-            title="ScanCard"
-            onPress = {() => navigation.navigate("ScanCard")}
-            />
-        </View>
-    )
+export default function Landing({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text h4>
+        Welcome to Hookt
+      </Text>
+      <View style={styles.buttonContainer}>
+        <Spacer>
+        <Button 
+          style={styles.button}
+          title="Signup"
+          onPress={() => navigation.navigate("Signup")} />
+          </Spacer>
+          <Spacer>
+        <Button 
+          style={styles.button}
+          title="Login"
+          onPress={() => navigation.navigate("Login")} />
+          </Spacer>
+          </View>
+      </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 2,
+    alignItems: "center",
+    justifyContent: "center",
+    width: '100%',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  },
+});
